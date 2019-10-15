9fcf8170f89fc5c0b2af45a10e8eb142715a97ac
DgbDiego <dgb.diego@gmail.com>
2019-10-15 13:55:27 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
