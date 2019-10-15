a48cb37bfdeb80fc997eeb7869c918fe07ceb19e
DgbDiego <dgb.diego@gmail.com>
2019-10-15 16:20:49 -0300

    1 file modified

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,17 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-    <nav class="navbar">
-
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <img src="assets/img/angular-logo.png">
-        </a>
-      </div>
-    </nav>
-  `,
-  styles: []
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
